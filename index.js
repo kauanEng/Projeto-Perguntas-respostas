397c03a70520786bc20f4f1c469a729caeb63542
Kauan santos <kauansantos83@gmail.com>
2020-08-29 11:22:35 -0300

    Iniciando os trabalhos

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.get("/:nome/:lang",(req, res) => {
+    res.render("index");
+});
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+
+});
+
+app.listen(8888,()=>{console.log("App rodando");});
