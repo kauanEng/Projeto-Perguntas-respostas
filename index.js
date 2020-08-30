8b14ed0111220d05c201671dafb8f51653a23ce0
Kauan santos <kauansantos83@gmail.com>
2020-08-30 16:11:39 -0300

    Body parser com configuração

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 
+//estou dizendo para o express usar o EJS como view engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req, res) => {
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
+app.get("/",(req, res) => {
     res.render("index");
 });
 
 app.get("/perguntar",(req, res) => {
     res.render("perguntar");
 
+
+});
+
+app.post("/salvarpergunta",(req, res)=> {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulario recebido! titulo " + titulo + " " + "descricao" + descricao);
 });
 
 app.listen(8888,()=>{console.log("App rodando");});
